6c38dce75d541b4a899f2098404fdecdf2cd75db
Gabriel Carvalho <gabriel.dossantoscarvalho@hotmail.com>
2019-01-04 12:34:24 -0200

    86. Tela de cadastro de usuários - recebendo e validando dados

--- mmorpg_got/app/controllers/cadastro.js
+++ mmorpg_got/app/controllers/cadastro.js
@@ -1,3 +1,26 @@
+
 module.exports.cadastro = function( application, req, res ){
-	res.render('cadastro');
+	res.render('cadastro', {validacao: {}, dadosForm: {}});
+};
+
+module.exports.cadastrar = function( application, req, res ){
+	let dadosForm = req.body;
+
+	req.assert('nome',		'Nome não pode ser vazio').notEmpty();
+	req.assert('usuario',	'Usuário não pode ser vazio').notEmpty();
+	req.assert('senha',		'Senha não pode ser vazio').notEmpty();
+	req.assert('casa',		'Casa não pode ser vazio').notEmpty();
+
+	let errosForm = req.validationErrors();
+
+	if ( errosForm ) {
+		res.render('cadastro', {validacao: errosForm, dadosForm: dadosForm});
+		// console.log(errosForm);
+		// res.send(`Existem erros no formulário.<br><br>${errosForm.reduce(function(acc, value){
+		// 	return ( acc instanceof Object )? acc.msg : `${acc}<br>${value.msg}` ;
+		// })}`);
+		return;
+	}
+
+	res.send("Podem cadastrar");
 };
