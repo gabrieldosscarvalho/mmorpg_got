a14a8eea633ae2ebc6e9efe666cac05783eff100
Gabriel Carvalho <gabriel.dossantoscarvalho@hotmail.com>
2021-05-24 12:25:35 -0300

    95. Configurando o Express-Session em nossa aplicação e 96. Autenticando usuários com Express-Session - parte 1

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,19 +1,24 @@
-module.exports.index = function( application, req, res ){
-	res.render('index', {validacao: {}});
+module.exports.index = function (application, req, res) {
+  res.render("index", { validacao: {} });
 };
 
-module.exports.autenticar = function( application, req, res ){
-	var dadosForm = req.body;
+module.exports.autenticar = function (application, req, res) {
+  var dadosForm = req.body;
 
-	req.assert( 'usuario',	'Usuário não deve ser vazio' ).notEmpty();
-	req.assert( 'senha',	'Senha não deve ser vazio' ).notEmpty();
+  req.assert("usuario", "Usuário não deve ser vazio").notEmpty();
+  req.assert("senha", "Senha não deve ser vazio").notEmpty();
 
-	var erros = req.validationErrors();
+  var erros = req.validationErrors();
 
-	if( erros ){
-		res.render('index', {validacao: erros});
-		return;
-	}
+  if (erros) {
+    res.render("index", { validacao: erros });
+    return;
+  }
 
-	res.send('ok')
-};
+  var connection = application.config.dbConnection;
+  var UsuarioDAO = new application.app.models.UsuariosDAO(connection);
+
+  UsuarioDAO.autenticar(dadosForm, req, res);
+
+      //   res.send("ok");
+};;
--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -1,18 +1,55 @@
-function UsuariosDAO( connection ){
-	this._connection = connection();
+function UsuariosDAO(connection) {
+  this._connection = connection();
+}
+
+UsuariosDAO.prototype.inserirUsuario = function (usuarioJson) {
+  console.log(this._connection);
+  this._connection.open(function (err, mongoClient) {
+    mongoClient.collection("usuarios", function (err, collection) {
+      collection.insert(usuarioJson);
+
+      mongoClient.close();
+    });
+  });
 };
 
-UsuariosDAO.prototype.inserirUsuario = function( usuarioJson ) {
-	console.log(this._connection);
-	this._connection.open( function( err, mongoClient ) {
-		mongoClient.collection( "usuarios", function( err, collection ){
-			collection.insert( usuarioJson );
+UsuariosDAO.prototype.autenticar = function (usuario, req, res) {
+  console.log("-- autenticar");
+  console.table(usuario);
+  this._connection.open(function (err, mongoClient) {
+    mongoClient.collection("usuarios", function (err, collection) {
+      var cursor = collection.find({
+        usuario: { $eq: usuario.usuario },
+        senha: { $eq: usuario.senha },
+      });
 
-			mongoClient.close();
-		} );
-	} );
+      //   **** TMBM PODERIA SER ASSIM ****
+      //   var cursor = collection.find({
+      //     usuario: usuario.usuario,
+      //     senha: usuario.senha,
+      //   });
+      //   **** CONSEQUENTEMENTE TMBM PODERIA SER ASSIM ****
+      //   var cursor = collection.find(usuario);
+
+      cursor.toArray(function (err, result) {
+        if (result[0] !== undefined) {
+          req.session.autorizado = true;
+          req.session.usuario = result[0].usuario;
+          req.session.casa = result[0].casa;
+        }
+
+        if (req.session.autorizado) {
+          res.redirect("jogo");
+        } else {
+          res.render("index", { validacao: {} });
+        }
+      });
+
+      mongoClient.close();
+    });
+  });
 };
 
-module.exports = function(){
-	return UsuariosDAO;
+module.exports = function () {
+  return UsuariosDAO;
 };
