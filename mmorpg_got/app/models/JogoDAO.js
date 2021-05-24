0f927507afcf9e257d230f256c7f4846adb7fcb2
Gabriel Carvalho <gabriel.dossantoscarvalho@hotmail.com>
2021-05-24 17:00:04 -0300

    102. Exibindo pontos de habilidades, súditos e moedas

--- mmorpg_got/app/models/JogoDAO.js
+++ mmorpg_got/app/models/JogoDAO.js
@@ -20,6 +20,21 @@ JogoDAO.prototype.gerarParametros = function (usuario) {
   });
 };
 
+JogoDAO.prototype.iniciaJogo = function (res, usuario, casa) {
+  this._connection.open(function (err, mongoClient) {
+    mongoClient.collection("jogo", function (err, collection) {
+      collection.find({ usuario: usuario }).toArray(function (err, result) {
+        console.log("-- USUARIO ENCONTRADO");
+        console.log(result[0]);
+
+        res.render("jogo", { img_casa: casa, jogo: result[0] });
+      });
+
+      mongoClient.close();
+    });
+  });
+};
+
 module.exports = function () {
   return JogoDAO;
 };
